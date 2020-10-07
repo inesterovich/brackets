69735dbeaf0276d668892e767d8574e65491c6ce
Ilya Nesterovich <manager@inesterovich.ru>
2020-10-07 20:28:07 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const brackets = Object.fromEntries(bracketsConfig);
+  const strArray = str.split('');
+ 
+  for(const element of strArray) {
+      if (stack[0] === element) {
+         stack.shift();
+      } else if (brackets[element]) {
+          stack.unshift(brackets[element])
+      } else {
+          return false;
+      }
+  }
+ 
+  return stack.length === 0;
+
+
 }
